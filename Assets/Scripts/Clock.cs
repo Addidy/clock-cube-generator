092b23ce031a83e8cdffe438fb42f9907e9e2fb9
Adam Davies <adamd94@hotmail.com>
2018-02-20 11:57:15 +0000

    created utc clock in correct format

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Clock : MonoBehaviour {
+
+    public DateTime time;
+    private Text text;
+
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time = DateTime.UtcNow;
+        text.text = time.ToString("HH:mm:ss");
+	}
+}
