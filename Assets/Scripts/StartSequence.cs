a5ef7654b41863ddf34435d7504f26b5b514af2c
Adam Davies <adamd94@hotmail.com>
2018-02-20 13:41:56 +0000

    Challenge requirements complete

---
+++ Assets/Scripts/StartSequence.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartSequence : MonoBehaviour {
+
+    public string nextScene;
+    private bool activated = false;
+
+    public void Activate() {
+        if (!activated)
+        {
+            activated = true;
+            GetComponent<Animation>().Play();
+        }
+    }
+
+    public void nextSceneOpen() {
+        SceneManager.LoadScene(nextScene, LoadSceneMode.Single);
+    }
+}
